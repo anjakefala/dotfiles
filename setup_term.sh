a7d929ea2b714a41ea9af2d1842366c09766d8cb
anjakefala <anja.kefala@gmail.com>
2017-10-22 22:12:30 +0000

    Add dotfiles

---
+++ setup_term.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+sudo apt-get update -y && sudo apt-get install -y tmux zsh vim git python3 python3-pip exuberant-ctags
+
+# get external tools used
+
+if [ ! -x ~/dotfiles ] ; then
+    git clone https://github.com/anjakefala/dotfiles.git ~/dotfiles
+fi
+
+cd ~/dotfiles
+
+# Should I submodule add oh-my-zsh
+git submodule update --init --recursive
+
+# setup .vim dirs
+
+mkdir -p ~/.vim/undo
+mkdir -p ~/.vim/swp
+
+# install actual dot files
+
+for i in zshrc visdatarc vimrc tmux.conf ctags gitconfig bash_aliases ; do
+    ln -sf `pwd`/$i ~/.$i
+done
+
+# change shell to zsh
+chsh -s $(grep /zsh$ /etc/shells | tail -1)
